879065805251fb01ff2817498ac0162ec50f38fd
Prelang Builder <builder@prelang.com>
2016-11-13 07:21:22 +0000

    Installed relation 'belongs_to' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
   #->Prelang (user_login:devise/username_login_support)
   belongs_to :representante
   belongs_to :produto
+  belongs_to :fabricante
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
