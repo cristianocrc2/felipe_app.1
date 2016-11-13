7608d1536d3d9166c9e9e000627b57587acd7778
Prelang Builder <builder@prelang.com>
2016-11-13 07:21:37 +0000

    Generated scaffolding for model 'Representante' via Rails

---
+++ app/views/representantes/_representante.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! representante, :id, :fabricante_id, :created_at, :updated_at
+json.url representante_url(representante, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :representantes
+
   resources :produtos do
   #->Prelang (voting/acts_as_votable)
   member do
