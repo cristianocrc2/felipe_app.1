c68e9bb3490a52eabc0a992c30e2d5a0a9952cdf
Prelang Builder <builder@prelang.com>
2016-11-13 07:21:34 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,5 +1,5 @@
 class ProdutosController < ApplicationController
-  before_action :set_produto, only: [:show, :edit, :update, :destroy]
+  before_action :set_produto, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /produtos
   # GET /produtos.json
